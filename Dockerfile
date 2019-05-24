4bc1dafdc8e0735707933df68363d06ca7c016e9
root <root@pigardendown.fritz.box>
2019-05-24 08:29:47 +0100

    update base image and script

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
-FROM hypriot/rpi-python
+FROM hickey/rpi-python3
 
-RUN apt-get update && \
-    apt-get -y install gcc && \
+RUN apt-get update && apt-get install -y \
+    python3-setuptools \
+    gcc \
+    --no-install-recommends && \
     rm -rf /var/lib/apt/lists/*
-RUN pip install paho-mqtt -i https://pypi.python.org/simple
-RUN pip install rpi.gpio -i https://pypi.python.org/simple
+
+RUN pip3 install paho-mqtt -i https://pypi.python.org/simple
+RUN pip3 install rpi.gpio -i https://pypi.python.org/simple
 
 ADD subscribe.py /var/jumajumo/subscribe.py
 RUN chmod +x /var/jumajumo/subscribe.py
 
-CMD python /var/jumajumo/subscribe.py
+CMD python3 /var/jumajumo/subscribe.py
--- subscribe.py
+++ subscribe.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import paho.mqtt.client as mqtt
-import RPi._GPIO as gpio
+import RPi.GPIO as gpio
 import time
 import os
 import datetime
@@ -12,31 +12,35 @@ pin = int(os.getenv('pin', '17'))
 thingTopic = "jumajumo/" + thingid + "/"
 commandTopic = thingTopic + "command"
 
-def on_connect(client, userdata, flags, rc):
-    client.subscribe(commandTopic, qos=1)
-    
+ON=gpio.LOW
+OFF=gpio.HIGH
+
+gpio.setmode(gpio.BCM)
+gpio.setup(pin,gpio.OUT)
+gpio.output(pin,OFF)
+
 def on_message(client, userdata, message):
 
-   if str(message.topic) == str(commandTopic):
-      msgReceived = str(message.payload)
-        
-      if str(message.payload) == str("ON"):
-         gpio.output(pin, 1)
+   if message.topic == commandTopic:
+      msgReceived = message.payload
+      if "ON" == msgReceived.decode():
+         gpio.output(pin,ON)
          time.sleep(1)
-         gpio.output(pin, 0)
-            
-         client.publish(commandTopic, "OFF", qos=1, retain=True)
+         gpio.output(pin,OFF)
+         client.publish(commandTopic, "OFF")
 
 client = mqtt.Client(thingid)
 
-client.will_set(thingTopic, "undef", qos=1, retain=True)
-client.on_connect=on_connect
-client.on_message=on_message 
+client.on_message=on_message
 
 client.connect(brokeraddr)
 
+client.subscribe(commandTopic)
+
+client.publish(commandTopic, "ONLINE")
 client.publish(thingTopic, str(datetime.datetime.now()), qos=1, retain=True)
 client.publish(thingTopic + "sys/type", "actor", qos=1, retain=True)
 client.publish(thingTopic + "sys/device", "relais-button", qos=1, retain=True)
 
 client.loop_forever()
+
